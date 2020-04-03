7c974def666be89ead8bc3c8b1193efbe830cdd7
pranaykrpiyush <pkp5214@gmail.com>
2020-04-03 20:28:39 +0530

    reveal remove

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 function expand(idName){
-    document.querySelector('#'+ idName).classList.add('reveal');
+  //  document.querySelector('#'+ idName).classList.remove('reveal');
+   
+  if (idName=="BEE"){
+  
+    document.querySelector('#BEC' ).classList.remove('reveal');
+    document.querySelector('#CHEM' ).classList.remove('reveal');
+    document.querySelector('#EVS' ).classList.remove('reveal');
+    document.querySelector('#CP' ).classList.remove('reveal');
+
+}
+else if (idName=="BEC")
+{
+    document.querySelector('#BEE' ).classList.remove('reveal');
+    document.querySelector('#CHEM' ).classList.remove('reveal');
+    document.querySelector('#EVS' ).classList.remove('reveal');
+    document.querySelector('#CP' ).classList.remove('reveal');
+}
+
+else if (idName=="CP")
+{
+    document.querySelector('#BEE' ).classList.remove('reveal');
+    document.querySelector('#CHEM' ).classList.remove('reveal');
+    document.querySelector('#EVS' ).classList.remove('reveal');
+    document.querySelector('#BEC' ).classList.remove('reveal');
+}
+else if (idName=="EVS")
+{
+    document.querySelector('#BEE' ).classList.remove('reveal');
+    document.querySelector('#CHEM' ).classList.remove('reveal');
+    document.querySelector('#BEC' ).classList.remove('reveal');
+    document.querySelector('#CP' ).classList.remove('reveal');
+}
+else if (idName=="CHEM")
+{
+    document.querySelector('#BEE' ).classList.remove('reveal');
+    document.querySelector('#BEC' ).classList.remove('reveal');
+    document.querySelector('#EVS' ).classList.remove('reveal');
+    document.querySelector('#CP' ).classList.remove('reveal');
+}
+
+
+
+    document.querySelector('#'+ idName).classList.toggle('reveal');
 }
